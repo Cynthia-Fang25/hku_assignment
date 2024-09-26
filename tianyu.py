d8fc4f0731ca402aaa7e286057ccd7160ade16bc
Tianyu Fang <145355905+Cynthia-Fang25@users.noreply.github.com>
2024-09-26 20:56:29 +0800

    drawing

---
+++ tianyu.py
@@ -0,0 +1,21 @@
+import turtle
+
+t = turtle.Turtle()
+t.forward(100)
+t.right(90)
+t.forward(75)
+t.right(90)
+t.forward(50)
+t.left(120)
+t.forward(75)
+t.left(-120)
+t.forward(25)
+t.right(60)
+t.forward(75)
+t.left(150)
+t.forward(70)
+t.left(-90)
+t.forward(25)
+t.right(90)
+t.forward(145)
+
