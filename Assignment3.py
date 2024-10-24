828bc32195706874c8df27dd93bff5a6646d8496
Tianyu Fang <145355905+Cynthia-Fang25@users.noreply.github.com>
2024-10-24 19:51:23 +0800

    Add files via upload
    assignment3

---
+++ Assignment3.py
@@ -0,0 +1,17 @@
+#from jetson_inference import detectNet 
+#from jetson_utils import videoSource, videoutput 
+import jetson.inference 
+import jetson.utils
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = jetson.utils.videoSource("/home/jetson-inference/data/images/dog_2.jpg")
+display = jetson.utils.videoOutput ("display://0") 
+while display.IsStreaming():
+	img = camera.Capture()
+	if img is None:
+		continue
+
+	detections=net.Detect(img)
+	print(detections[0])
+	display.Render (img)
+	display. SetStatus("Object Detection / Network {:.0f} FPS". format(net.GetNetworkFPS()))
